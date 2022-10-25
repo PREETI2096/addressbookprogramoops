2cad9b24ec085fd4ac6dcf6c6223563ac7e967c0
PREETI2096 <preetirg96@gmail.com>
2022-10-26 01:10:10 +0530

    [preeti]:added uc1 of address book program

---
+++ src/com/bridgelabz/addressbookprogramoops/AddressbookMain.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.addressbookprogramoops;
+
+public class AddressbookMain {
+
+	public static void main(String[] args) {
+	System.out.println("Welcome to address book program...");
+
+	}
+
+}
